f9dec94a0a8f5a1123cb48a9d51fccc445302b39
PabloSgreccia <pablosgreccia@gmail.com>
2018-04-12 17:56:15 -0300

    Update ejercicio-04.py

--- practico-01/ejercicio-04.py
+++ practico-01/ejercicio-04.py
@@ -3,11 +3,13 @@
 print("Ejercicio 4: Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario devuelve False.\n")
 
 def esVocal(a):
-    if ord(a) == 65 or  ord(a) == 69 or ord(a) == 73 or  ord(a) == 79 or ord(a) == 85 or  ord(a) == 97 or  ord(a) == 101 or  ord(a) == 105 or  ord(a) == 111 or  ord(a) == 117:
+    if a in vocales:
         return True
 
 a = "f"
-if esVocal(a):
+vocales = ['a','e','i','o','u']
+
+if esVocal(a.lower()):
     print("La letra ",a, " es una vocal")
 else:
     print("La letra ",a, " es una consonante")
