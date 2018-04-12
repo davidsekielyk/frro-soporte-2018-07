d695be3c697ad0718dad874870697a51c3f8ce71
PabloSgreccia <pablosgreccia@gmail.com>
2018-04-12 18:24:11 -0300

    Update ejercicio-02.py

--- practico-01/ejercicio-02.py
+++ practico-01/ejercicio-02.py
@@ -14,3 +14,7 @@ b = 234
 c = 663
 
 print("EL maximo entre ",a, ", ",b, " y ",c, "es: ",max_de_tres(a, b, c))
+
+assert max_de_tres(a, b, c) == a
+assert max_de_tres(a, b, c) == b
+assert max_de_tres(a, b, c) == c
