286e4600bb363e14a64056542e724e4905811f43
davidsekielyk <30438751+davidsekielyk@users.noreply.github.com>
2018-07-25 18:09:36 -0300

    correcion testing ejercicio-03

--- practico_05/test-ejercicio-03.py
+++ practico_05/test-ejercicio-03.py
@@ -2,96 +2,40 @@ from practico_05.ejercicio_01 import Socio
 from practico_05.ejercicio_03 import DatosSocio
 
 
-def opMenu():
-    print("Ingrese una de las siguientes opciones")
-    print("1-Alta")
-    print("2-Borrar")
-    print("3-Modificar")
-    print("4-Buscar")
-    print("5-Todos")
-    print("6-Cantidad de DNI")
-    print("0-Terminar")
-    global op
-    b = True
-    while b:
-        try:
-            op = int(input("Opcion: "))
-            b = False
-        except:
-            print("Ingresa un numero (-.-) ")
+def pruebas():
 
-def nuevoSocio():
-    ns = Socio()
-    print("\nIngrese los datos de un nuevo socio:")
-    ns.idSoci = int(input("ID: "))
-    ns.dni = int(input("DNI: "))
-    ns.nombre = input("Nombre: ")
-    ns.apellido = input("Apellido: ")
-    return ns
+    # alta
+    datos = DatosSocio()
+    socio = Socio(idSoci=111, dni=12345678, nombre="Juan", apellido="Perez")
+    b = datos.alta(socio)
+    assert b[0] == True
 
-def viejoSocio():
-    id = int(input("\nIngresa id del socio que quieres eliminar: "))
-    vs = ds.buscar(id)
-    return vs
+    # baja
+    assert datos.borrar(socio)[0] == True
 
-def modificarSocio():
-    ms = Socio()
-    ms.idSoci = int(input("Ingrese ID del socio que quiere modificar: "))
-    print("\nIngrese los datos que quiere modificar del socio:")
-    ms.dni = int(input("DNI: "))
-    ms.nombre = input("Nombre: ")
-    ms.apellido = input("Apellido: ")
-    return ms
+    # buscar
+    socio_2 = Socio(idSoci=112, dni=12345679, nombre='Carlos', apellido='Perez')
+    datos.alta(socio_2)
+    assert datos.buscar(socio_2.idSoci) == socio_2
+
+    # modificacion
+    socio_3 = Socio(idSoci=113, dni=12345680, nombre='Susana', apellido='Gimenez')
+    datos.alta(socio_3)
+    socio_3_modif = datos.buscar(socio_3.idSoci)
+    socio_3_modif.nombre = 'Moria'
+    socio_3_modif.apellido = 'Casan'
+    socio_3_modif.dni = 13264587
+    b = datos.modificar(socio_3_modif)
+    assert b[0] == True
+
+    # todos
+    assert len(datos.todos()) == 2
+
+    # buscar dni
+    socio_4 = Socio(idSoci=114, dni=12345678, nombre='Carlos', apellido='Perez')
+    datos.alta(socio_4)
+    assert datos.buscarDNI(socio_4.dni) == 1
 
 
-ds = DatosSocio()
-opMenu()
-while op != 0:
-    if op == 1:
-        ns = nuevoSocio()
-        b = ds.alta(ns)
-        if b[0]:
-            print("Se ha registrado un nuevo socio\n")
-        else:
-            print("\nYa existe esa persona")
-            print("ERROR:", b[1].args[0])
-    elif op == 2:
-        vs = viejoSocio()
-        b = ds.borrar(vs)
-        if b[0]:
-            print("Se ha eliminado un socio\n")
-        else:
-            print("\nNo se ha podido eliminar el socio")
-            print("ERROR:", b[1].args[0])
-    elif op == 3:
-        ms = modificarSocio()
-        b = ds.modificar(ms)
-        if b[0]:
-            print("Se ha modificado un socio\n")
-        else:
-            print("\nNo se ha podido modificar el socio")
-            print("ERROR:", b[1].args[0])
-    elif op == 4:
-        id = int(input("\nIngresa id del socio que quieres buscar: "))
-        soc = ds.buscar(id)
-        print("ID: ", soc.idSoci)
-        print("DNI: ", soc.dni)
-        print("Nombre: ", soc.nombre)
-        print("Apellido: ", soc.apellido)
-        print("-------------------------------------------")
-    elif op == 5:
-        ls = ds.todos()
-        print('\nLista de personas:')
-        for p in ls:
-            print("ID: ", p.idSoci)
-            print("DNI: ", p.dni)
-            print("Nombre: ", p.nombre)
-            print("Apellido: ", p.apellido)
-            print("--------------------------------------------")
-    elif op == 6:
-        dni = int(input("Ingrese DNI: "))
-        cantDni = ds.buscarDNI(dni)
-        print("Cantidad de DNI: ", cantDni)
-    else:
-        print("\nHDP elegi una de esas opciones\n")
-    opMenu()
+if __name__ == '__main__':
+    pruebas()
